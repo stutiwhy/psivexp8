02b974679bc0d12a78ca578b3d2ccf7089b67e91
MehwishSiddiquie <mehwishsiddiquie20@gmail.com>
2026-09-11 11:27:29 +0530

    fix

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 def train():
     # Load dataset
     iris = load_iris()
-    X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=42)
     
     # Train lightweight model
     clf = RandomForestClassifier(n_estimators=10, random_state=42)
